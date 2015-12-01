39af6d353ae6b1205ec7609f26fc773e65977813
chabare <chabare95@gmail.com>
2015-12-01 20:38:47 +0100

    Don't update main after this

--- main.go
+++ main.go
@@ -2,38 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/chabare/sql/sql"
-	"github.com/ziutek/mymysql/mysql"
-	_ "github.com/ziutek/mymysql/native" // Native engine
+	"github.com/chabare/SQLGolang/cSql"
+	_ "github.com/ziutek/mymySql/native" // Native engine
 )
 
-func createConnection(host, port, user, pass, name string) mysql.Conn {
-	d := mysql.New("tcp", "", host+":"+port, user, pass, name)
-	err := d.Connect()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return d
-}
-
 func main() {
-	cnff := sql.ConfigFile{"config.ini"}
+	cnff := cSql.ConfigFile{"config.ini"}
 	cnf := cnff.GetConfig()
 
-	conn := createConnection(cnf.Host, cnf.Port, cnf.User, cnf.Pass, cnf.Name)
+	conn := *cSql.CreateConnection(cnf.Host, cnf.Port, cnf.User, cnf.Pass, cnf.Name)
 	defer conn.Close()
-	folder := sql.DBTable{"Folder", []string{"ID", "Name", "Count", "Size", "LastIterationID", "Created"}}
 
-	// values := []string{"asd", "50", "50", "1", "2015-11-29"}
-	cnd := []sql.Condition{sql.Condition{"ID", "5"}}
+	folder := cSql.DBTable{"Folder", []string{"ID", "Name", "Count", "Size", "LastIterationID", "Created"}}
+	// values := []string{"asd", "50", "50", "1", "2015-11-29"})
+	cnd := []cSql.Condition{{"ID", "5"}}
 
-	cols := []string{"Count", "Size"}
-	vals := []string{"20", "20"}
-	r := folder.Update(&conn, &cols, &vals, &cnd)
+	//cols := []string{"Count", "Size"}
+	//vals := []string{"20", "20"}
+	r, _ := folder.Select(&conn, &[]string{}, &cnd)
 
-	fmt.Println(r.AffectedRows())
+	fmt.Println(r[0].Str(0))
 }
